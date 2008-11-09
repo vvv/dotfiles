c3e8bad825a59348a3e1c7e153eebc39bed96372
Valery V. Vorotyntsev <valery.vv@gmail.com>
2008-11-09 14:27:01 +0200

    show percentage (and time in parentheses)

--- bin/battery-dzen.pl
+++ bin/battery-dzen.pl
@@ -3,41 +3,55 @@
 use strict;
 use warnings;
 
-sub numval ($) {
+sub openR ($) { ### open file for reading
+    my ($name) = @_;
+    open my $f, '<', $name or die "Can't read file: $!";
+    return $f;
+}
+
+sub line2num ($) { ### read line, get a number from it
     my ($f) = @_;
     $_ = <$f>; s/\D+//g; return $_;
 }
 
-open my $f, '<', '/proc/acpi/battery/BAT1/state'
-    or die "Can't read file: $!";
+sub battery_params() { ### read battery parameters
+    my $f = openR '/proc/acpi/battery/BAT1/state';
+    scalar <$f>; scalar <$f>;
 
-scalar <$f>; scalar <$f>;
+    $_ = <$f>; /\S+$/; my $ch_state = $&; # `charging state'
+    exit 0 if $ch_state eq 'charged';
 
-$_ = <$f>; /\S+$/; my $charging_state = $&;
-exit 0 if $charging_state eq 'charged';
-
-my $present_rate = numval $f;
-my $remaining_capacity = numval $f;
-close $f;
-
-print "|| $charging_state: ";
-
-if ($charging_state eq 'discharging') {
-    my $ml = $remaining_capacity * 60 / $present_rate; # minutes left
-    if ($ml < 20) { print '^fg(red)' . int($ml) . '^fg()'; }
-    elsif ($ml < 40) { print '^fg(green)' . int($ml) . '^fg()'; }
-    elsif ($ml < 60) { print int($ml); }
-    else { printf "%d:%02d", (int($ml / 60), int($ml % 60)); }
-}
-
-if ($charging_state eq 'charging') {
-    open my $f, '<', '/proc/acpi/battery/BAT1/info'
-	or die "Can't read file: $!";
-    scalar <$f>;
-    my $design_capacity = numval $f;
+    my $rate = line2num $f;    # `present rate'
+    my $rem_cap = line2num $f; # `remaining capacity'
     close $f;
 
-    print int(100 * $remaining_capacity / $design_capacity) . '%';
+    $f = openR '/proc/acpi/battery/BAT1/info';
+    scalar <$f>;
+    my $des_cap = line2num $f; # `design capacity'
+    close $f;
+
+    return ($ch_state, $rate, $rem_cap, $des_cap);
 }
 
-print " \n";
+my ($ch_state, $rate, $rem_cap, $des_cap) = &battery_params;
+
+my ($minutes, $color) = (0.0, '');
+my $report = int(100 * $rem_cap / $des_cap) . '% (';
+
+if ($ch_state eq 'charging') {
+    $minutes = 60 * ($des_cap - $rem_cap) / $rate;
+} else { # discharging
+    $minutes = 60 * $rem_cap / $rate;
+    $report .= '~';
+    if ($minutes < 20) { $color = 'red'; }
+    elsif ($minutes < 40) { $color = 'green'; }
+}
+
+if ($minutes < 60) {
+    $report .= int($minutes) . "')";
+} else {
+    $report .= sprintf '%d:%02d)', (int($minutes / 60), int($minutes % 60));
+}
+if ($color) { $report = "^fg($color)" . $report . "^fg()"; }
+
+print "|| $ch_state: $report \n";
