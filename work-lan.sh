fab62c209f3de88690eb002aa0c01b92a9d3e762
Valery V. Vorotyntsev <valery.vv@gmail.com>
2009-03-17 18:09:09 +0200

    interfaces (umc-static): new logical interface
    Faster network startup.

--- work-lan.sh
+++ work-lan.sh
@@ -1,6 +1,7 @@
 /usr/local/bin/work-lan-p.sh && return
 
-sudo ifup eth0 || return
+#sudo ifup eth0=umc-dynamic || return
+sudo ifup eth0=umc-static || return
 
 . /usr/local/bin/work-proxy-vars.sh
 sudo -E /etc/init.d/lastfmsubmitd restart
