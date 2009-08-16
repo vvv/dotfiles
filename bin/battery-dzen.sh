f78dc2050a665e0fbb20659e102a566776521bcf
Valery V. Vorotyntsev <valery.vv@gmail.com>
2009-08-16 23:38:44 +0300

    battery-dzen.sh: new [/sys/class/power_supply]
    New battery meter works with /sys/class/power_supply/BAT1/ files.
    (There is no /proc/acpi/battery/BAT1 in 2.6.30 kernel.)
    
    BUGS: I'm not sure whether "time left" estimation works correctly.

---
+++ bin/battery-dzen.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+BAT=/sys/class/power_supply/BAT1
+cd $BAT 2>/dev/null || {
+    echo "`basename $0`: cannot access $BAT/" 1>&2
+    exit 1
+}
+
+STATUS=$(cat status)
+CURRENT=$(cat current_now)
+CAPACITY=$(cat energy_full_design)
+REMAINING=$(cat energy_now)
+
+REPORT="$(($REMAINING * 100 / $CAPACITY))% ("
+
+if [ "$STATUS" = 'Discharging' ]; then
+    MINS=$((60 * $REMAINING / $CURRENT))
+    REPORT="${REPORT}~"
+    if [ $MINS -lt 20 ]; then
+	COLOR='red'
+    elif [ $MINS -lt 40 ]; then
+	COLOR='green'
+    fi
+else
+    MINS=$((60 * ($CAPACITY - $REMAINING) / $CURRENT))
+    STATUS='+'
+fi
+
+if [ $MINS -lt 60 ]; then
+    REPORT="${REPORT}$MINS')"
+else
+    REPORT="${REPORT}$(printf "%d:%02d\n" $(($MINS / 60)) $(($MINS % 60))))"
+fi
+
+REPORT="$STATUS $REPORT"
+[ -n "$COLOR" ] && REPORT="^fg($COLOR)$REPORT^fg()"
+
+echo "|| $REPORT "
