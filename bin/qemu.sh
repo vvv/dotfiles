8b4f01dc536a540d6296247bbd08f08e7d3137c1
Valery V. Vorotyntsev <valery.vv@gmail.com>
2010-03-29 22:31:47 +0300

    xmpp-notify: fixed bug in date format
    qemu.sh: add `downscript=no' parameter to `-net'

--- bin/qemu.sh
+++ bin/qemu.sh
@@ -4,5 +4,5 @@ set -e
 
 grep -q '^tap0=' /etc/network/run/ifstate || sudo ifup tap0
 
-kvm -m 256 -localtime -usb -net nic -net tap,ifname=tap0,script=no \
-    -hda ~/hd.raw "$@"
+kvm -m 256 -localtime -usb -net nic \
+    -net tap,ifname=tap0,script=no,downscript=no -hda ~/hd.raw "$@"
