a9c70c0d45e83bcd8b428b6165d789671e597e34
Valery V. Vorotyntsev <valery.vv@gmai.com>
2011-08-11 11:56:43 +0300

    logt-minutes.pl: LogT format has been changed

--- bin/logt-minutes.pl
+++ bin/logt-minutes.pl
@@ -8,31 +8,40 @@ use Time::Local;
 my %logt = ();
 my $stopped = 0;
 my $prev_time = 0;
+my $accum_time = 0;
 
 while (<>) {
     die "Line $.: Invalid input\n" unless
-	#           1       2      3      4      5      6       7
-	(/^(?:\w+ )?(\d{4})-(\d\d)-(\d\d) (\d\d):(\d\d):(\d\d)> (\S+)(?:\s+--.*)?$/);
+	#           1       2      3      4      5      6       7     8
+	(/^(?:\w+ )?(\d{4})-(\d\d)-(\d\d) (\d\d):(\d\d):(\d\d)> ([io])( .*)?$/);
 
     my $t = timelocal($6, $5, $4, $3, $2, $1);
     die "Line $.: Timestamp is out of order\n"
 	if $prev_time != 0 && $t >= $prev_time;
     die "Impossible happened" if $t == 0;
 
-    if ($7 eq '.') {
+    if ($7 eq 'o') {
 	die "Line $.: Two stops in row\n" if $stopped;
 	$stopped = 1;
     } else {
-	die "Line $.: \"Stop\" (.) expected\n" if $prev_time == 0;
-
-	$logt{$7} += $prev_time - $t;
+	die "Line $.: \"Stop\" (o) expected\n" if $prev_time == 0;
 	$stopped = 0;
+
+	if ($8) {
+	    my $task = $8;
+	    $task =~ s/^\s*(.+)\s*$/$1/; # strip of {lead,trail}ing whitespace
+	    $logt{$task} += $accum_time + $prev_time - $t;
+	    $accum_time = 0;
+	} else {
+	    $accum_time += $prev_time - $t;
+	}
     }
 
     $prev_time = $t;
 }
 
-die "LogT must not end with \"stop\" (.)\n" if ($stopped);
+die "LogT must not end with \"stop\" (o)\n" if ($stopped);
+die "Task name is missing at the end of LogT\n" if ($accum_time != 0);
 
 sub show_time($$) {
     my $s = shift;
