b488a5f2c20cb7c1a76a69cd50e6cbde1ef94d90
Valery V. Vorotyntsev <valery.vv@gmai.com>
2011-08-15 17:10:36 +0300

    logt-minutes.pl: support comments and empty lines

--- bin/logt-minutes.pl
+++ bin/logt-minutes.pl
@@ -11,6 +11,11 @@ my $prev_time = 0;
 my $accum_time = 0;
 
 while (<>) {
+    $_ =~ s/--.*$//;  # strip of comments
+    $_ =~ s/^\s+//;   # strip of leading whitespace
+    $_ =~ s/\s+$//;   # strip of trailing whitespace
+    next unless ($_); # skip empty lines
+
     die "Line $.: Invalid input\n" unless
 	#           1       2      3      4      5      6       7     8
 	(/^(?:\w+ )?(\d{4})-(\d\d)-(\d\d) (\d\d):(\d\d):(\d\d)> ([io])( .*)?$/);
@@ -29,7 +34,7 @@ while (<>) {
 
 	if ($8) {
 	    my $task = $8;
-	    $task =~ s/^\s*(.+)\s*$/$1/; # strip of {lead,trail}ing whitespace
+	    $task =~ s/^\s*(.+)$/$1/; # strip of leading whitespace
 	    $logt{$task} += $accum_time + $prev_time - $t;
 	    $accum_time = 0;
 	} else {
