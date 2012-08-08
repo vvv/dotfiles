f3c3ab943d40f13341b92151503c15f264c285c1
Valery V. Vorotyntsev <valery.vv@gmail.com>
2012-08-08 12:51:43 +0300

    bin/magic-gen.pl: magic numbers generator

---
+++ bin/magic-gen.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use List::Util qw(shuffle);
+
+my $auto_p = 0;
+if (defined $ARGV[0]) {
+    die "Usage: $0 [--auto]\n" unless
+	$#ARGV == 0 and ($ARGV[0] eq '-a' or $ARGV[0] eq '--auto');
+    $auto_p = 1;
+}
+
+my $value = '';
+my $mnemonics = '';
+
+open(my $words, '<', '/usr/share/dict/words') or die;
+foreach (shuffle <$words>) {
+    next unless $_ =~ /^[a-fols]+$/i;
+    chomp;
+    tr/A-Z/a-z/;  # convert to lower case
+
+    unless ($auto_p) {
+	$mnemonics = $_;
+	tr/ols/015/;
+	print length($_) . " $mnemonics $_\n";
+	next;
+    }
+
+    $value .= $_;
+    $mnemonics .= " $_";
+    if (length $value >= 16) {
+	$value =~ tr/ols/015/;
+	print substr($value, 0, 16)
+	    . substr($mnemonics, 0, length($mnemonics) + 16 - length($value))
+	    . "\n";
+	$value = $mnemonics = '';
+    }
+}
+close $words;
