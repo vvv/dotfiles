db31f2f3302b5f161bfc0b92e68f487e0a940674
Valery V. Vorotyntsev <valery.vv@gmail.com>
2012-09-06 01:05:03 +0300

    magic-gen.pl: delete "--auto" mode; accept 'z'
    - `magic-gen.py' script[1] does better job at automatic magic generation.
    - Accept 'z' symbol and convert it to '2'.
    - Make some code improvements as suggested[2] by Dima Chumak[3]. Thanks!
    
      [1] https://github.com/vvv/dotfiles/blob/master/bin/magic-gen.py
      [2] https://gist.github.com/3507432
      [3] https://github.com/chumakd

--- bin/magic-gen.pl
+++ bin/magic-gen.pl
@@ -4,37 +4,18 @@ use strict;
 use warnings;
 use List::Util qw(shuffle);
 
-my $auto_p = 0;
-if (defined $ARGV[0]) {
-    die "Usage: $0 [--auto]\n" unless
-	$#ARGV == 0 and ($ARGV[0] eq '-a' or $ARGV[0] eq '--auto');
-    $auto_p = 1;
-}
-
 my $value = '';
 my $mnemonics = '';
 
 open(my $words, '<', '/usr/share/dict/words') or die;
 foreach (shuffle <$words>) {
-    next unless $_ =~ /^[a-foils]+$/i;
+    next unless /^[a-foilsz]+$/i;
+
     chomp;
     tr/A-Z/a-z/;  # convert to lower case
 
-    unless ($auto_p) {
-	$mnemonics = $_;
-	tr/oils/0115/;
-	print length($_) . " $mnemonics $_\n";
-	next;
-    }
-
-    $value .= $_;
-    $mnemonics .= " $_";
-    if (length $value >= 16) {
-	$value =~ tr/oils/0115/;
-	print substr($value, 0, 16)
-	    . substr($mnemonics, 0, length($mnemonics) + 16 - length($value))
-	    . "\n";
-	$value = $mnemonics = '';
-    }
+    $mnemonics = $_;
+    tr/oilsz/0115z/;
+    print length . " $mnemonics $_\n";
 }
 close $words;
