187ba2272ca0c7b112667b05ed13bcd9d8f75136
Valery V. Vorotyntsev <valery.vv@gmail.com>
2021-05-12 01:07:48 +0300

    .doom.d: change default theme on Linux

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -33,9 +33,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
-(when IS-MAC
-  (setq doom-theme 'doom-solarized-dark))
+;(setq doom-theme 'doom-one)
+(setq doom-theme (if IS-MAC 'doom-solarized-dark 'doom-tomorrow-night))
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
